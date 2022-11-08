429407837135515b9b087012afbd5ea672a19e64
Jaroslav Hejny <jaroslav.hejny@ringieraxelspringer.sk>
2022-11-08 15:30:00 +0100

    events

--- error.js
+++ error.js
@@ -1,3 +1,4 @@
+import EVENT_TYPES from './event_types'
 class ExamplePlugin {
     constructor(opts, root, video) {
         this.opts = opts;
@@ -12,16 +13,28 @@ class ExamplePlugin {
         var $ = flowplayer.mq;
 
         // render a custom button when the player is mounted
-        this.video.on('mount', (e) => {
+        this.video.on(EVENT_TYPES.MOUNT, (e) => {
             window.parent.postMessage('ring-mount','*');
         });
-        this.video.on('pause', (e) => {
+        this.video.on(EVENT_TYPES.PAUSE, (e) => {
             window.parent.postMessage('ring-pause','*');
         });
-        this.video.on('playing', (e) => {
+        this.video.on(EVENT_TYPES.PLAYING, (e) => {
             window.parent.postMessage('ring-playing','*');
         });
-        this.video.on('RING_VIDEO_DETAILS_LOAD_ERROR', (e) => {
+        this.video.on(EVENT_TYPES.ENDED, (e) => {
+            window.parent.postMessage('ring-ended','*');
+        });
+        this.video.on(EVENT_TYPES.SEEKED, (e) => {
+            window.parent.postMessage('ring-seeked','*');
+        });
+        this.video.on(EVENT_TYPES.PROGRESS, (e) => {
+            window.parent.postMessage('ring-progress','*');
+        });
+        this.video.on(EVENT_TYPES.VOLUME_CHANGE, (e) => {
+            window.parent.postMessage('ring-volumechange','*');
+        });
+        this.video.on(EVENT_TYPES.RING_VIDEO_DETAILS_LOAD_ERROR, (e) => {
             window.parent.postMessage('RING_VIDEO_DETAILS_LOAD_ERROR','*');
         });
     }
---
+++ event_types.js
@@ -0,0 +1,29 @@
+const EVENT_TYPES = {
+    FULLSCREEN_ENTER: 'fullscreen_enter',
+    FULLSCREEN_EXIT: 'fullscreen_exit',
+    ENDED: 'ended',
+    ERROR: 'error',
+    DATA: 'data',
+    METADATA: 'metadata',
+    LOAD_START: 'load_start',
+    MOUNT: 'mount',
+    PAUSE: 'pause',
+    PLAYING: 'playing',
+    PROGRESS: 'progress',
+    RESIZE: 'resize',
+    SEEKED: 'seeked',
+    SEEKING: 'seeking',
+    SOURCE: 'source',
+    TIME_UPDATE: 'time_update',
+    VIEW_ENTER: 'view_enter',
+    VIEW_LEAVE: 'view_leave',
+    VOLUME_CHANGE: 'volume_change',
+    WAITING: 'waiting',
+    CUEPOINTS: 'cuepoints',
+    CUEPOINT_START: 'cuepoint_start',
+    CUEPOINT_END: 'cuepoint_end',
+    REAP: 'fp:reap',
+    RING_VIDEO_DETAILS_LOAD_ERROR: 'RING_VIDEO_DETAILS_LOAD_ERROR'
+}
+
+export default EVENT_TYPES
