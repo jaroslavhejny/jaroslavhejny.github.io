05a85fdd23ab303075f42da4e5a4414957835c0d
Jaroslav Hejny <jaroslav.hejny@ringieraxelspringer.sk>
2022-11-08 15:00:13 +0100

    test

--- error.js
+++ error.js
@@ -7,18 +7,23 @@ class ExamplePlugin {
 
     init() {
         this.video.on('mount', (e) => {
+            console.log('mount');
             window.postMessage({data: 'video-mounted'},'*');
         });
         this.video.on('RING_VIDEO_DETAILS_LOAD_ERROR', (e) => {
+            console.log('RING_VIDEO_DETAILS_LOAD_ERROR');
             window.postMessage('RING_VIDEO_DETAILS_LOAD_ERROR','*');
         });
         this.video.on('pause', (e) => {
+            console.log('pause');
             window.postMessage('pause','*');
         });
         this.video.on('seeked', (e) => {
+            console.log('seeked');
             window.postMessage('seeked','*');
         });
         this.video.on('playing', (e) => {
+            console.log('playing');
             window.postMessage('ended','*');
         });
     }
