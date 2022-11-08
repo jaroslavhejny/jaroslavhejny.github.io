2e643199276cc8105388906491524fd5a2ee2f9e
Jaroslav Hejny <jaroslav.hejny@ringieraxelspringer.sk>
2022-11-08 12:20:36 +0100

    test

---
+++ error.js
@@ -0,0 +1,25 @@
+export default class ExamplePlugin {
+    constructor(opts, root, video) {
+        this.opts = opts;
+        this.root = root;
+        this.video = video;
+    }
+
+    init() {
+        // small jQuery-compatible utility built inside the player
+        var $ = flowplayer.mq
+
+        // render a custom button when the player is mounted
+        this.video.on('mount', () => {
+            var btn = $('<button>').txt('A custom mute button').css("background-color", "red").on('click', () => {
+                this.video.toggleMute();
+            })
+            $('.fp-primary', this.root).prepend(btn);
+        })
+    }
+}
+
+flowplayer((opts, root, video) => {
+    const plugin = new ExamplePlugin(opts, root, video);
+    plugin.init();
+});
