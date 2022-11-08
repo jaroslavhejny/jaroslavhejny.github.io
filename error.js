8398c1be043646776f561602a64f1e15a4cec38c
Jaroslav Hejny <jaroslav.hejny@ringieraxelspringer.sk>
2022-11-08 14:26:11 +0100

    test

--- error.js
+++ error.js
@@ -6,14 +6,8 @@ class ExamplePlugin {
     }
 
     init() {
-        console.log('loadPlugin');
-        // small jQuery-compatible utility built inside the player
-
-        var $ = flowplayer.mq;
-
-        // render a custom button when the player is mounted
         this.video.on('mount', (e) => {
-            window.postMessage('RING_VIDEO_DETAILS_LOAD_ERROR','*');
+            window.postMessage({data: 'video-mounted'},'*');
         });
         this.video.on('RING_VIDEO_DETAILS_LOAD_ERROR', (e) => {
             window.postMessage('RING_VIDEO_DETAILS_LOAD_ERROR','*');
