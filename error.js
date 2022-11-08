ca787ae0a26e9c01357d3859637f02d8f31a8158
Jaroslav Hejny <jaroslav.hejny@ringieraxelspringer.sk>
2022-11-08 13:59:04 +0100

    test

--- error.js
+++ error.js
@@ -13,16 +13,7 @@ class ExamplePlugin {
 
         // render a custom button when the player is mounted
         this.video.on('mount', (e) => {
-            console.log('mount');
-            const event = new CustomEvent('ring-mount', {
-                detail: e.detail,
-                bubbles: true
-            });
-            this.video.dispatchEvent(event);
-            window.dispatchEvent(event);
-            document.dispatchEvent(event);
             window.postMessage('ring-mount','*');
-            window.parent.postMessage('ring-mount','*');
         });
         this.video.on('RING_VIDEO_DETAILS_LOAD_ERROR', (e) => {
             console.log('RING_VIDEO_DETAILS_LOAD_ERROR', e.detail);
