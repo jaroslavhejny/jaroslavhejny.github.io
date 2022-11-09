024d40fd1f8457ce288c3409dd2485402a004795
Jaroslav Hejny <jaroslav.hejny@ringieraxelspringer.sk>
2022-11-09 10:21:38 +0100

    remove duplicated listeners

--- error.js
+++ error.js
@@ -7,23 +7,17 @@ class ExamplePlugin {
 
     init() {
         console.log('loadPlugin');
-        // small jQuery-compatible utility built inside the player
-
-        var $ = flowplayer.mq;
-
-        // render a custom button when the player is mounted
         this.video.on('mount', (e) => {
-            window.parent.postMessage('ring-mount','*');
-            window.parent.postMessage('RING_VIDEO_DETAILS_LOAD_ERROR','*');
+            window.parent.postMessage('ring-mount', '*');
         });
         this.video.on('pause', (e) => {
-            window.parent.postMessage('ring-pause','*');
+            window.parent.postMessage('ring-pause', '*');
         });
         this.video.on('playing', (e) => {
-            window.parent.postMessage('ring-playing','*');
+            window.parent.postMessage('ring-playing', '*');
         });
         this.video.on('ended', (e) => {
-            window.parent.postMessage('ring-ended','*');
+            window.parent.postMessage('ring-ended', '*');
         });
         this.video.on('seeked', (e) => {
             // window.parent.postMessage('ring-seeked','*');
@@ -32,10 +26,10 @@ class ExamplePlugin {
             // window.parent.postMessage('ring-progress','*');
         });
         this.video.on('volume_change', (e) => {
-            window.parent.postMessage('ring-volumechange','*');
+            window.parent.postMessage('ring-volumechange', '*');
         });
         this.video.on('RING_VIDEO_DETAILS_LOAD_ERROR', (e) => {
-            window.parent.postMessage('RING_VIDEO_DETAILS_LOAD_ERROR','*');
+            window.parent.postMessage('RING_VIDEO_DETAILS_LOAD_ERROR', '*');
         });
     }
 }
