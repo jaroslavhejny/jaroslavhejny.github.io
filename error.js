6fb7b6a8a281f7866e0babfc28aa8e9c64bd5ec7
Branislav Zagrapan <branislav.zagrapan@ringier.sk>
2023-01-24 09:34:56 +0100

    test

--- error.js
+++ error.js
@@ -1,40 +1,40 @@
 class ExamplePlugin {
-    constructor(opts, root, video) {
-        this.opts = opts;
-        this.root = root;
-        this.video = video;
-    }
+  constructor(opts, root, video) {
+    this.opts = opts;
+    this.root = root;
+    this.video = video;
+  }
 
-    init() {
-        console.log('loadPlugin');
-        this.video.on('mount', (e) => {
-            window.parent.postMessage('ring-mount', '*');
-        });
-        this.video.on('pause', (e) => {
-            window.parent.postMessage('ring-pause', '*');
-        });
-        this.video.on('playing', (e) => {
-            window.parent.postMessage('ring-playing', '*');
-        });
-        this.video.on('ended', (e) => {
-            window.parent.postMessage('ring-ended', '*');
-        });
-        this.video.on('seeked', (e) => {
-            // window.parent.postMessage('ring-seeked','*');
-        });
-        this.video.on('progress', (e) => {
-            // window.parent.postMessage('ring-progress','*');
-        });
-        this.video.on('volume_change', (e) => {
-            window.parent.postMessage('ring-volumechange', '*');
-        });
-        this.video.on('RING_VIDEO_DETAILS_LOAD_ERROR', (e) => {
-            window.parent.postMessage('RING_VIDEO_DETAILS_LOAD_ERROR', '*');
-        });
-    }
+  init() {
+    console.log("Brekeke");
+    this.video.on("mount", (e) => {
+      window.parent.postMessage("ring-mount", "*");
+    });
+    this.video.on("pause", (e) => {
+      window.parent.postMessage("ring-pause", "*");
+    });
+    this.video.on("playing", (e) => {
+      window.parent.postMessage("ring-playing", "*");
+    });
+    this.video.on("ended", (e) => {
+      window.parent.postMessage("ring-ended", "*");
+    });
+    this.video.on("seeked", (e) => {
+      // window.parent.postMessage('ring-seeked','*');
+    });
+    this.video.on("progress", (e) => {
+      // window.parent.postMessage('ring-progress','*');
+    });
+    this.video.on("volume_change", (e) => {
+      window.parent.postMessage("ring-volumechange", "*");
+    });
+    this.video.on("RING_VIDEO_DETAILS_LOAD_ERROR", (e) => {
+      window.parent.postMessage("RING_VIDEO_DETAILS_LOAD_ERROR", "*");
+    });
+  }
 }
 
 flowplayer((opts, root, video) => {
-    const plugin = new ExamplePlugin(opts, root, video);
-    plugin.init();
+  const plugin = new ExamplePlugin(opts, root, video);
+  plugin.init();
 });
