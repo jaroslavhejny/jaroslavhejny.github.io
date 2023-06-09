b23925fa2239879dacb298477e1ddc9bd9f7addd
Jaroslav Hejny <jaroslav.hejny@ringieraxelspringer.sk>
2023-06-09 15:56:38 +0200

    styling

--- denik/script.js
+++ denik/script.js
@@ -5,14 +5,29 @@ const urlInput = document.getElementById('denik');
 const containsDenik = (value) => {
     return value.indexOf('.denik.') > -1;
 }
+const DANGER = 'btn-danger';
+const SUCCESS = 'btn-success';
+const DISABLED = 'disabled'
+
+const toggleBtn = (value) => {
+    if (value) {
+        generateButton.classList.remove(DANGER);
+        generateButton.classList.add(SUCCESS);
+        generateButton.attributes.removeNamedItem(DISABLED);
+    } else {
+        generateButton.classList.remove(SUCCESS);
+        generateButton.classList.add(DANGER);
+        generateButton.setAttribute(DISABLED, DISABLED)
+    }
+}
 pasteButton.addEventListener('click', async () => {
     try {
         const text = await navigator.clipboard.readText();
         const isDenik = containsDenik(text);
-        if (isDenik){
+        if (isDenik) {
             urlInput.value = text;
-        }
-        else {
+            toggleBtn(true);
+        } else {
             alert('Není url z deníku!');
         }
 
@@ -20,13 +35,13 @@ pasteButton.addEventListener('click', async () => {
         console.log('Failed to read clipboard');
     }
 });
-generateButton.addEventListener('click', ()=>{
+generateButton.addEventListener('click', () => {
     const url = document.getElementById('denik').value;
     const newUrl = new URL(url);
     const finalUrl = `${newUrl.origin}${newUrl.pathname}?demoseznam`
     window.open(finalUrl, "_blank");
 })
 
-urlInput.addEventListener('input', (value)=>{
-    console.log(value);
+urlInput.addEventListener('input', () => {
+    toggleBtn(containsDenik(urlInput.value));
 })
